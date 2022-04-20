cbce51df320287f5015b564e08de4a9599d70671
Sena <darkphoenixq@gmail.com>
2022-04-21 01:17:33 +0300

    commit

--- src/main/java/com/sena/transactionservice/controller/TransactionController.java
+++ src/main/java/com/sena/transactionservice/controller/TransactionController.java
@@ -1,9 +1,11 @@
 package com.sena.transactionservice.controller;
 
+import com.sena.transactionservice.model.Transaction;
 import com.sena.transactionservice.service.ITransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/transaction")
@@ -11,4 +13,19 @@ public class TransactionController {
 
     @Autowired
     private ITransactionService transactionService;
+
+    @PostMapping
+    public ResponseEntity<?> saveTransaction(@RequestBody Transaction transaction) {
+        return new ResponseEntity<>(transactionService.saveTransaction(transaction), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteTransaction(@PathVariable Long transactionId) {
+        transactionService.deleteTransaction(transactionId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    @GetMapping
+    public ResponseEntity<?> findAllTransaction(@PathVariable Long userId){
+        return ResponseEntity.ok(transactionService.findAllTransaction(userId));
+    }
 }
